14b9e3d83b618ba4bcbd5254f8de34c1a80a170c
Bogdan Yarema <b.yarema@mobimill.com>
2018-03-06 17:44:30 +0200

    add base for rest api in laravel

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /* Add the fillable property into the Product Model */
+    protected $fillable = ['title', 'description', 'price', 'availability'];
+
+
 }
